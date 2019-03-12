14179f29b4a9b82f4df153d41e2ae60c808065a1
Tommy Boshkovski <boshkovski.tommy@gmail.com>
2019-03-12 15:04:16 -0400

    js change

--- site.js
+++ site.js
@@ -1,30 +1,4 @@
 
-$(function() {
-
-    // Call Gridder
-    $('.gridder').gridderExpander({
-        scroll: true,
-        scrollOffset: 30,
-        scrollTo: "panel",                  // panel or listitem
-        animationSpeed: 400,
-        animationEasing: "easeInOutExpo",
-        showNav: true,                      // Show Navigation
-        nextText: "Next",                   // Next button text
-        prevText: "Previous",               // Previous button text
-        closeText: "Close",                 // Close button text
-        onStart: function(){
-            //Gridder Inititialized
-        },
-        onContent: function(){
-            //Gridder Content Loaded
-        },
-        onClosed: function(){
-            //Gridder Closed
-        }
-    });
-
-});
-
 $.extend($.easing,
 {
     def: 'easeOutQuad',
@@ -137,4 +111,34 @@ $(document).ready(function (){
       }]
     });
   });
+});
+
+
+
+
+
+$(function() {
+
+    // Call Gridder
+    $('.gridder').gridderExpander({
+        scroll: true,
+        scrollOffset: 30,
+        scrollTo: "panel",                  // panel or listitem
+        animationSpeed: 400,
+        animationEasing: "easeInOutExpo",
+        showNav: true,                      // Show Navigation
+        nextText: "Next",                   // Next button text
+        prevText: "Previous",               // Previous button text
+        closeText: "Close",                 // Close button text
+        onStart: function(){
+            //Gridder Inititialized
+        },
+        onContent: function(){
+            //Gridder Content Loaded
+        },
+        onClosed: function(){
+            //Gridder Closed
+        }
+    });
+
 });
