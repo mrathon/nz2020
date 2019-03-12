efda0ed9a5986c69a0977de0af8f1f0228f7db1d
Tommy Boshkovski <boshkovski.tommy@gmail.com>
2019-03-12 15:06:38 -0400

    js change

--- site.js
+++ site.js
@@ -120,7 +120,7 @@ $(document).ready(function (){
 $(function() {
 
     // Call Gridder
-    $('.gridder').gridderExpander({
+    $.gridder.gridderExpander({
         scroll: true,
         scrollOffset: 30,
         scrollTo: "panel",                  // panel or listitem
