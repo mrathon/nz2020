e6ec0c019bf3b30c978e1cf8d7bb11da019e5b60
Tommy Boshkovski <boshkovski.tommy@gmail.com>
2019-03-12 15:23:20 -0400

    ..

--- site.js
+++ site.js
@@ -117,28 +117,29 @@ $(document).ready(function (){
 
 
 
-$(function() {
+
 
     // Call Gridder
-    $.gridder.gridderExpander({
-        scroll: true,
-        scrollOffset: 30,
-        scrollTo: "panel",                  // panel or listitem
-        animationSpeed: 400,
-        animationEasing: "easeInOutExpo",
-        showNav: true,                      // Show Navigation
-        nextText: "Next",                   // Next button text
-        prevText: "Previous",               // Previous button text
-        closeText: "Close",                 // Close button text
-        onStart: function(){
-            //Gridder Inititialized
-        },
-        onContent: function(){
-            //Gridder Content Loaded
-        },
-        onClosed: function(){
-            //Gridder Closed
-        }
-    });
-
-});
+    $('.gridder').gridderExpander({
+                               scroll: true,
+                                   scrollOffset: 30,
+                                   scrollTo: "panel", // panel or listitem
+                                   animationSpeed: 400,
+                                   animationEasing: "easeInOutExpo",
+                                   showNav: true, // Show Navigation
+                                   nextText: "", // Next button text
+                                   prevText: "", // Previous button text
+                                   closeText: "", // Close button text
+                                   onStart: function () {
+                                       //Gridder Inititialized
+                                       console.log('On Gridder Initialized...');
+                                   },
+                                   onContent: function () {
+                                       //Gridder Content Loaded
+                                       console.log('On Gridder Expand...');
+                                   },
+                                   onClosed: function () {
+                                       //Gridder Closed
+                                       console.log('On Gridder Closed...');
+                                   }
+                               });
