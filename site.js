1bfe0c503e8f949af8365317f68f3937c6f7d6a7
Tommy Boshkovski <boshkovski.tommy@gmail.com>
2019-03-12 14:23:13 -0400

    gridder js

--- site.js
+++ site.js
@@ -1,4 +1,30 @@
 
+$(function() {
+
+    // Call Gridder
+    $('.gridder').gridderExpander({
+        scroll: true,
+        scrollOffset: 30,
+        scrollTo: "panel",                  // panel or listitem
+        animationSpeed: 400,
+        animationEasing: "easeInOutExpo",
+        showNav: true,                      // Show Navigation
+        nextText: "Next",                   // Next button text
+        prevText: "Previous",               // Previous button text
+        closeText: "Close",                 // Close button text
+        onStart: function(){
+            //Gridder Inititialized
+        },
+        onContent: function(){
+            //Gridder Content Loaded
+        },
+        onClosed: function(){
+            //Gridder Closed
+        }
+    });
+
+});
+
 $.extend($.easing,
 {
     def: 'easeOutQuad',
@@ -112,4 +138,3 @@ $(document).ready(function (){
     });
   });
 });
-
